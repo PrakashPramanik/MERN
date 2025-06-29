66d8e63e12d464c97b939407af3bdf7b095ed64b
Prakash Pramanik <130439023+PrakashPramanik@users.noreply.github.com>
2025-06-29 22:29:56 +0530

    Add files via upload

---
+++ 27-06-2025/day8.1.js
@@ -0,0 +1,32 @@
+// arr=[1,2,3,4,5,6,7]
+// const res= arr.filter((ele)=>{
+//     arr.pop()
+//     return true
+// })
+
+// console.log(res)
+
+
+// const total = arr.reduce((sum,curr)=>{
+//     return curr+sum
+//     // return 8
+// },1000)
+
+// console.log(total)
+
+
+// ajax
+
+
+
+// num=prompt("enter a number")
+// let regex = /^\d{10}$/
+
+// if(regex.test(num)){
+//     console.log("valid")
+// }
+// else{
+//     console.log("invalid")
+// }
+
+
